360a17651e6597a3142507fa83a6e1da9e79b336
Bulgakov Egor <egor_mrag1999@,ail.ru>
2023-02-27 23:44:37 +0300

    pravka

--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -5,9 +5,10 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Needle7 {
-
+    @Autowired
+    private Deth8 deth8;
     @Override
     public String toString() {
-        return ", смерть Кощея на игле :( " + "";
+        return ", смерть Кощея на игле :( " + "" + deth8.toString();
     }
 }
